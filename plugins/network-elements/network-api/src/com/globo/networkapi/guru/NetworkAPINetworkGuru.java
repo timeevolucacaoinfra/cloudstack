37af31002bc13b0db9d8db860240f96713ac780c
Silvano Buback <silvano@corp.globo.com>
2014-01-10 17:03:46 -0200

    first implementation of networkapi network plugin

---
+++ plugins/network-elements/network-api/src/com/globo/networkapi/guru/NetworkAPINetworkGuru.java
@@ -0,0 +1,37 @@
+package com.globo.networkapi.guru;
+
+import org.apache.log4j.Logger;
+
+import com.cloud.dc.DataCenter.NetworkType;
+import com.cloud.network.Network;
+import com.cloud.network.PhysicalNetwork;
+import com.cloud.network.Network.GuestType;
+import com.cloud.network.Network.Service;
+import com.cloud.network.PhysicalNetwork.IsolationMethod;
+import com.cloud.network.guru.GuestNetworkGuru;
+import com.cloud.offering.NetworkOffering;
+
+public class NetworkAPINetworkGuru extends GuestNetworkGuru {
+    private static final Logger s_logger = Logger.getLogger(NetworkAPINetworkGuru.class);
+    
+    public NetworkAPINetworkGuru() {
+        _isolationMethods = new IsolationMethod[] { IsolationMethod.VLAN };
+    }
+
+	@Override
+	protected boolean canHandle(NetworkOffering offering,
+			NetworkType networkType, PhysicalNetwork physicalNetwork) {
+		s_logger.trace("Asking for handling network with offering " + offering +
+				" network type " + networkType + " and physical network " + physicalNetwork);
+        // This guru handles only Guest Isolated network that supports Source nat service
+        if (networkType == NetworkType.Advanced
+                && isMyTrafficType(offering.getTrafficType()) 
+                && isMyIsolationMethod(physicalNetwork)) {
+            return true;
+        } else {
+            s_logger.trace("We only take care of Guest networks of type   " + GuestType.Shared + " in zone of type " + NetworkType.Advanced);
+            return false;
+        }
+	}
+
+}
