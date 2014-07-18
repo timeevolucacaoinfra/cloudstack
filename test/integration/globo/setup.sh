9e1dd11da871ac38aeae4822a53f7d30a46e10c0
Luis Henrique Okama <okama@corp.globo.com>
2014-07-18 16:00:18 -0300

    .

--- test/integration/globo/setup.sh
+++ test/integration/globo/setup.sh
@@ -13,7 +13,7 @@
 VBoxManage='/usr/bin/VBoxManage'
 vbox_host='10.2.162.11'
 vbox_user='vbox'
-vbox_vm_ci_string='CI.*DevCloud2.*Clone4'
+vbox_vm_ci_string='DevCloud2 original'
 
 snapshot_use_name='ci-ready'
 virtualenv_name='cloudstack'
@@ -22,7 +22,7 @@ project_basedir='/var/lib/jenkins/cloudstack'
 project_branch='4.3.0-globo'
 maven_log='/tmp/cloudstack.log'
 
-devcloud_host='10.2.162.19'
+devcloud_host='192.168.56.10'
 
 debug=1
 
@@ -48,19 +48,19 @@ TestConnection() {
 ShutdownVM() {
     vm_uuid=$1
     [[ -n $2 ]] && shutdowncmd='poweroff' || shutdowncmd='acpipowerbutton'
-    [[ $debug ]] && PrintLog DEBUG "VBoxManage controlvm ${vm_uuid} ${shutdowncmd}"
+    [[ $debug ]] && PrintLog INFO "VBoxManage controlvm ${vm_uuid} ${shutdowncmd}"
     cmd_output=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} controlvm ${vm_uuid} ${shutdowncmd}")
 }
 
 RestoreSnapshot() {
     vm_uuid=$1
-    [[ $debug ]] && PrintLog DEBUG "RemoteSshCmd VBoxManage --nologo snapshot ${vm_uuid} restore $snapshot_use_name"
+    [[ $debug ]] && PrintLog INFO "VBoxManage --nologo snapshot ${vm_uuid} restore $snapshot_use_name"
     cmd_output=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo snapshot ${vm_uuid} restore $snapshot_use_name")
 }
 
 TurnOnVM() {
     vm_uuid=$1
-    [[ $debug ]] && PrintLog DEBUG "VBoxManage startvm ${vm_uuid}"
+    [[ $debug ]] && PrintLog INFO "VBoxManage startvm ${vm_uuid}"
     cmd_output=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo startvm ${vm_uuid} --type headless")
 }
 
@@ -71,7 +71,7 @@ ListVM() {
 
 WaitforJettyReady() {
     max_retries=15
-    sleep_time=8
+    sleep_time=10
     ret_count=1
     [[ $debug ]] && PrintLog DEBUG "Checking if jetty is ready..."
     [[ ! -f $maven_log ]] && sleep 5
@@ -98,10 +98,11 @@ WaitForVMState() {
     while [ $ret_count -le $max_retries ]; do
         vm_status=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo showvminfo ${vm_uuid} | awk '/State/ {print \$2}'")
         if [[ $vm_status = $wait_for_state ]]; then
-            [[ $debug ]] && echo "[DEBUG] VM state is already in '${wait_for_state}'"
+            [[ $debug ]] && PrintLog DEBUG "VM state is already in '${wait_for_state}' state"
             return 1
         else
-            [[ $debug ]] && echo -ne "[DEBUG] VM state ${vm_uuid} in '${vm_status}' state, still waiting for '${wait_for_state}' state... sleeping more ${sleep_time}(s) count ${ret_count}/${max_retries}\r"
+            # arrumar esse echo
+            [[ $debug ]] && PrintLog DEBUG "VM state ${vm_uuid} is in '${vm_status}' state, still waiting for '${wait_for_state}' state... sleeping more ${sleep_time}(s) count ${ret_count}/${max_retries}"
             sleep $sleep_time
             ret_count=$[$ret_count+1]
         fi
@@ -115,18 +116,20 @@ WaitforSSH() {
     max_retries=8
     sleep_time=3
     ret_count=1
-    [[ $debug ]] && echo -ne "[DEBUG] Trying to connect to ${host_to_connect}..."
+    [[ $debug ]] && PrintLog DEBUG "Trying to connect to ${host_to_connect}..."
     while [ $ret_count -le $max_retries ]; do
         if echo "QUIT" | nc -w 2 ${host_to_connect} 22 >/dev/null 2>&1; then
-            [[ $debug ]] && echo "ok"
+            [[ $debug ]] && PrintLog INFO "SSH service is ok on host ${host_to_connect}"
             return 1
         else
-            [[ $debug ]] && echo -ne "not ready yet...sleeping more ${sleep_time}(s) count ${ret_count}/${max_retries}\r"
+            [[ $debug ]] && PrintLog DEBUG "SSH is not ready yet...sleeping more ${sleep_time}(s) count ${ret_count}/${max_retries}"
             sleep $sleep_time
             ret_count=$[$ret_count+1]
         fi
     done
-    return 0
+    # return 0
+    PrintLog ERROR "Ssh service is not running in vm ${vm_uuid}..."
+    exit 1
 }
 
 # Main
@@ -137,7 +140,7 @@ ListVM
 
 ShutdownVM $vm_uuid 
 WaitForVMState $vm_uuid powered
-[[ $debug ]] && PrintLog DEBUG "Removing log file '${maven_log}'"
+[[ $debug ]] && PrintLog INFO "Removing log file '${maven_log}'"
 rm -f ${maven_log}
 RestoreSnapshot $vm_uuid
 [[ $? -ne 0 ]] && PrintLog ERROR "Failed to restore snapshot '${snapshot_use_name} for vm ${vm_uuid}...exiting!" && exit 1
@@ -153,6 +156,7 @@ git checkout ${project_branch}
 source $WORKON_HOME/${virtualenv_name}/bin/activate
 [[ $debug ]] && PrintLog INFO "Creating SQL schema"
 mvn -q -P developer -pl developer,tools/devcloud -Ddeploydb >/dev/null 2>/dev/null
+[[ $? -ne 0 ]] && PrintLog ERROR "Failed to deploy DB" && exit 1
 [[ $debug ]] && PrintLog INFO "Doing some required SQL migrations"
 # mvn -P developer,systemvm clean install -DskipTests
 (cd setup/dbmigrate && db-migrate >/dev/null)
@@ -162,7 +166,9 @@ WaitforSSH ${devcloud_host}
 MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n" mvn --log-file ${maven_log} -pl :cloud-client-ui jetty:run >/dev/null &
 [[ $debug ]] && PrintLog INFO "Creating an advanced zone..."
 WaitforJettyReady
+# project_basedir='/var/lib/jenkins/cloudstack'
 # python ${project_basedir}/tools/marvin/marvin/deployDataCenter.py -i ${project_basedir}/test/integration/globo/cfg/devcloud.cfg
+# inserir alguns check para verificar a disponibilidade do ambiente
 # nosetests --with-marvin --marvin-config=demo.cfg --load test_dns_api.py
 echo "[INFO] Kill cloudstack..."
 # kill $(ps wwwaux | awk '/[j]ava.*cloud-client-ui jetty:run/ {print $2}')
