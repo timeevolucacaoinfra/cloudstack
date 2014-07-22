4e1985c4e11081c1daff9c6d9ded55fc54b93048
Luis Henrique Okama <okama@corp.globo.com>
2014-07-22 15:33:22 -0300

    use simulator hypervisor

--- test/integration/globo/setup.sh
+++ test/integration/globo/setup.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
-# ==================================================================
-##
-##  The user who will run this script, needs to have a ssh key pair on
-##  VirtualBox host
-##
-##
-##
-## 16/Jul/2014 - timeevolucaoinfra@corp.globo.com
-# ==================================================================
-# baixar a systemvms e restaurar a partir desse ponto
 
-VBoxManage='/usr/bin/VBoxManage'
-vbox_host='10.2.162.11'
-vbox_user='vbox'
-vbox_vm_ci_string='DevCloud2 original'
-
-snapshot_use_name='ci-ready'
 virtualenv_name='cloudstack'
 
 project_basedir='/var/lib/jenkins/cloudstack'
+globo_test_basedir="${project_basedir}/test/integration/globo"
 project_branch='4.3.0-globo'
 maven_log='/tmp/cloudstack.log'
-
-devcloud_host='192.168.56.10'
 
 debug=1
 
@@ -35,44 +18,12 @@ PrintLog() {
     echo "[${timestamp}] [${level}] ${msg}"
 }
 
-TestConnection() {
-    # check if ssh connection has no problem and get the Xen CI UUID
-    [[ $debug ]] && PrintLog DEBUG "Trying to connect to ${vbox_user}@${vbox_host}"
-    [[ $debug ]] && PrintLog DEBUG "ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l ${vbox_user} ${vbox_host}"
-    if ! ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host ":"; then
-        PrintLog ERROR "Failed to log in ${vbox_user}@${vbox_host}"
-        exit 1
-    fi
-}
-
-ShutdownVM() {
-    vm_uuid=$1
-    [[ -n $2 ]] && shutdowncmd='poweroff' || shutdowncmd='acpipowerbutton'
-    [[ $debug ]] && PrintLog INFO "VBoxManage controlvm ${vm_uuid} ${shutdowncmd}"
-    cmd_output=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} controlvm ${vm_uuid} ${shutdowncmd}")
-}
-
-RestoreSnapshot() {
-    vm_uuid=$1
-    [[ $debug ]] && PrintLog INFO "VBoxManage --nologo snapshot ${vm_uuid} restore $snapshot_use_name"
-    cmd_output=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo snapshot ${vm_uuid} restore $snapshot_use_name")
-}
-
-TurnOnVM() {
-    vm_uuid=$1
-    [[ $debug ]] && PrintLog INFO "VBoxManage startvm ${vm_uuid}"
-    cmd_output=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo startvm ${vm_uuid} --type headless")
-}
-
-ListVM() {
-    [[ $debug ]] && PrintLog DEBUG "VBoxManage --nologo list vms | grep -w '${vbox_vm_ci_string}' | grep -oP '\w+\-\w+\-\w+\-\w+\-\w+'"    
-    vm_uuid=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo list vms | grep -w '${vbox_vm_ci_string}' | grep -oP '\w+\-\w+\-\w+\-\w+\-\w+'")
-}
-
-WaitforJettyReady() {
+StartJetty() {
     max_retries=15
     sleep_time=10
     ret_count=1
+    PrintLog INFO "Starting cloudstack w/ simulator..."
+    MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n" mvn --log-file ${maven_log} -pl client jetty:run -Dsimulator >/dev/null &
     [[ $debug ]] && PrintLog DEBUG "Checking if jetty is ready..."
     [[ ! -f $maven_log ]] && sleep 5
     while [ $ret_count -le $max_retries ]; do
@@ -89,86 +40,87 @@ WaitforJettyReady() {
     exit 1
 }
 
-WaitForVMState() {
-    vm_uuid=$1
-    wait_for_state=$2
-    max_retries=10
-    sleep_time=5
-    ret_count=1
-    while [ $ret_count -le $max_retries ]; do
-        vm_status=$(ssh -q -oBatchMode=yes -oPasswordAuthentication=no -l $vbox_user $vbox_host "${VBoxManage} --nologo showvminfo ${vm_uuid} | awk '/State/ {print \$2}'")
-        if [[ $vm_status = $wait_for_state ]]; then
-            [[ $debug ]] && PrintLog DEBUG "VM state is already in '${wait_for_state}' state"
-            return 1
-        else
-            # arrumar esse echo
-            [[ $debug ]] && PrintLog DEBUG "VM state ${vm_uuid} is in '${vm_status}' state, still waiting for '${wait_for_state}' state... sleeping more ${sleep_time}(s) count ${ret_count}/${max_retries}"
-            sleep $sleep_time
-            ret_count=$[$ret_count+1]
-        fi
-    done
-    PrintLog ERROR "Vm ${vm_uuid} still in '${vm_status}' state, not in '${wait_for_state}' state"
-    exit 1
-}
-
-WaitforSSH() {
-    host_to_connect=$1
-    max_retries=8
+ShutdownJetty() {
+    max_retries=5
     sleep_time=3
     ret_count=1
-    [[ $debug ]] && PrintLog DEBUG "Trying to connect to ${host_to_connect}..."
+    PrintLog INFO "Stopping cloudstack..."
+    kill $(ps wwwaux | awk '/[m]aven.*jetty:run -Dsimulator/ {print $2}')
+    [[ $? -ne 0 ]] && PrintLog ERROR "Failed to stop jetty"
     while [ $ret_count -le $max_retries ]; do
-        if echo "QUIT" | nc -w 2 ${host_to_connect} 22 >/dev/null 2>&1; then
-            [[ $debug ]] && PrintLog INFO "SSH service is ok on host ${host_to_connect}"
+        if [[ -z $(ps wwwaux | awk '/[m]aven.*jetty:run -Dsimulator/ {print $2}') ]]; then
             return 1
         else
-            [[ $debug ]] && PrintLog DEBUG "SSH is not ready yet...sleeping more ${sleep_time}(s) count ${ret_count}/${max_retries}"
+            [[ $debug ]] && PrintLog DEBUG "Jetty is alive, waiting more ${sleep_time} (${ret_count}/${max_retries})"
             sleep $sleep_time
             ret_count=$[$ret_count+1]
         fi
     done
-    # return 0
-    PrintLog ERROR "Ssh service is not running in vm ${vm_uuid}..."
+    PrintLog WARN "Kill -9 to jetty process!!!"
+    kill -9 $(ps wwwaux | awk '/[m]aven.*jetty:run -Dsimulator/ {print $2}')
+}
+
+WaitForInfrastructure() {
+    max_retries=20
+    sleep_time=10
+    ret_count=1
+    PrintLog INFO "Waiting infrastructure..."
+    while [ $ret_count -le $max_retries ]; do
+        if grep -q 'server resources successfully discovered by SimulatorSecondaryDiscoverer' ${maven_log}; then
+            [[ $debug ]] && PrintLog INFO "Jetty is running and ready"
+            return 1
+        else
+            [[ $debug ]] && PrintLog DEBUG "Infrasctructure is not ready yet... sleeping more ${sleep_time}sec (${ret_count}/${max_retries})"
+            sleep $sleep_time
+            ret_count=$[$ret_count+1]
+        fi
+    done
+    PrintLog ERROR "Infrastructure was not ready in $((${max_retries}*${sleep_time})) seconds..."
     exit 1
 }
 
-# Main
-TestConnection
-ListVM
-[[ -z $vm_uuid ]] && PrintLog ERROR "There is no VM with string '${vbox_vm_ci_string}' on name... exiting!" && exit 1
-[[ $debug ]] && PrintLog DEBUG "VM UUID '${vm_uuid}'"
-
-ShutdownVM $vm_uuid 
-WaitForVMState $vm_uuid powered
-[[ $debug ]] && PrintLog INFO "Removing log file '${maven_log}'"
+# Checkout repository, compile, use virtualenv and sync the mavin commands
+PrintLog INFO "Removing log file '${maven_log}'"
 rm -f ${maven_log}
-RestoreSnapshot $vm_uuid
-[[ $? -ne 0 ]] && PrintLog ERROR "Failed to restore snapshot '${snapshot_use_name} for vm ${vm_uuid}...exiting!" && exit 1
-WaitForVMState $vm_uuid powered
-TurnOnVM $vm_uuid
 [[ $debug ]] && PrintLog DEBUG "Change work dir to ${project_basedir}"
-
 [[ ! -d $project_basedir ]] && PrintLog ERROR "Directory ${project_basedir} does not exist...exit" && exit 1
 cd ${project_basedir}
-[[ $debug ]] && PrintLog INFO "Checking out to branch '${project_branch}'"
-git checkout ${project_branch}
-[[ $debug ]] && PrintLog INFO "Switching to '${virtualenv_name}' virtualenv"
+PrintLog INFO "Checking out to branch '${project_branch}'"
+git checkout ${project_branch} >/dev/null 2>/dev/null
+PrintLog INFO "Switching to '${virtualenv_name}' virtualenv"
 source $WORKON_HOME/${virtualenv_name}/bin/activate
-[[ $debug ]] && PrintLog INFO "Creating SQL schema"
-mvn -q -P developer -pl developer,tools/devcloud -Ddeploydb >/dev/null 2>/dev/null
+# mvn -Pdeveloper -Dsimulator clean install
+# mvn -P developer -pl :cloud-marvin
+# mvn -Pdeveloper,marvin.sync -Dendpoint=localhost -pl :cloud-marvin
+
+# Deploy DB, Populate DB and create infra structure
+PrintLog INFO "Creating SQL schema"
+mvn -q -P developer -pl developer -Ddeploydb >/dev/null 2>/dev/null
 [[ $? -ne 0 ]] && PrintLog ERROR "Failed to deploy DB" && exit 1
-[[ $debug ]] && PrintLog INFO "Doing some required SQL migrations"
-# mvn -P developer,systemvm clean install -DskipTests
+mvn -Pdeveloper -pl developer -Ddeploydb-simulator >/dev/null 2>/dev/null
+[[ $? -ne 0 ]] && PrintLog ERROR "Failed to deploy DB simulator" && exit 1
+PrintLog INFO "Doing some required SQL migrations"
 (cd setup/dbmigrate && db-migrate >/dev/null)
-WaitForVMState $vm_uuid running
-WaitforSSH ${devcloud_host}
-[[ $debug ]] && PrintLog INFO "Starting cloudstack..."
-MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n" mvn --log-file ${maven_log} -pl :cloud-client-ui jetty:run >/dev/null &
-[[ $debug ]] && PrintLog INFO "Creating an advanced zone..."
-WaitforJettyReady
-# project_basedir='/var/lib/jenkins/cloudstack'
-# python ${project_basedir}/tools/marvin/marvin/deployDataCenter.py -i ${project_basedir}/test/integration/globo/cfg/devcloud.cfg
-# inserir alguns check para verificar a disponibilidade do ambiente
-# nosetests --with-marvin --marvin-config=demo.cfg --load test_dns_api.py
-echo "[INFO] Kill cloudstack..."
-# kill $(ps wwwaux | awk '/[j]ava.*cloud-client-ui jetty:run/ {print $2}')
+StartJetty
+PrintLog INFO "Creating an advanced zone..."
+python ${project_basedir}/tools/marvin/marvin/deployDataCenter.py -i ${project_basedir}/test/integration/globo/cfg/advanced-globo.cfg
+
+# Required restart
+WaitForInfrastructure
+ShutdownJetty
+PrintLog INFO "Removing log file '${maven_log}'"
+rm -f ${maven_log}
+StartJetty
+
+# Tests
+nosetests --with-marvin --marvin-config=${globo_test_basedir}/demo.cfg --load ${globo_test_basedir}/test_dns_api.py
+tail -1 $(ls -dtr /tmp/[0-9]* | tail -1)/results.txt | grep -qw 'OK'
+retval=$?
+if [[ $retval -eq 0 ]]; then
+    ShutdownJetty
+    PrintLog INFO "All steps and tests successfully passed"
+    exit 0
+else
+    PrintLog ERROR "Tests failed!!!"
+    exit 1
+fi
