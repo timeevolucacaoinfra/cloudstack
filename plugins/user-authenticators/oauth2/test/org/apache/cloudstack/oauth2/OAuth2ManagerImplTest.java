389e0297282c02c9bb8418b3e59ccedc0f4b761f
Daniel Vega <daniel.simoes@corp.globo.com>
2014-08-28 14:11:58 -0300

    Fixing tests

--- plugins/user-authenticators/oauth2/test/org/apache/cloudstack/oauth2/OAuth2ManagerImplTest.java
+++ plugins/user-authenticators/oauth2/test/org/apache/cloudstack/oauth2/OAuth2ManagerImplTest.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.URLEncoder;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.cloudstack.test.utils.SpringUtils;
 import org.junit.After;
@@ -28,6 +27,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+import com.cloud.domain.DomainVO;
 import com.cloud.user.DomainManager;
 import com.cloud.user.dao.UserAccountDao;
 import com.cloud.utils.component.ComponentContext;
@@ -35,13 +35,15 @@ import com.cloud.utils.component.ComponentContext;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
-public class OAuth2AuthenticatorTest {
+public class OAuth2ManagerImplTest {
 	
 	@Inject
 	private OAuth2ManagerImpl _oAuth2Auth;
 	
 	private final String AUTHLOCATION = "https://www.example.com/oauth/authorize";
 	private final String CLIENTID = "djAjpUQ53oXGY1SiUGFM";
+	private final String CLIENTSECRET = "jaskm9AKnmadsiFNUFNAlnk15kn";
+	private final String TOKENURL = "https://www.example.com/oauth/token";
 	
 	@Before
 	public void setUp() throws Exception {
@@ -50,6 +52,9 @@ public class OAuth2AuthenticatorTest {
 		
 		when(_oAuth2Auth.getAuthorizationURL()).thenReturn(AUTHLOCATION);
 		when(_oAuth2Auth.getClientID()).thenReturn(CLIENTID);
+		when(_oAuth2Auth.getClientSecret()).thenReturn(CLIENTSECRET);
+        when(_oAuth2Auth.getTokenURL()).thenReturn(TOKENURL);
+        when(_oAuth2Auth.getUserDomainVO()).thenReturn(new DomainVO());
 	}
 	
 	@After
@@ -64,23 +69,6 @@ public class OAuth2AuthenticatorTest {
 		String authUrl = _oAuth2Auth.generateAuthenticationUrl(returnUrl);
 		assertNotNull(authUrl);
 		assertEquals(AUTHLOCATION + "?response_type=code&redirect_uri=" + encodedUrl + "&client_id=" + CLIENTID, authUrl);
-	}
-	
-	@Test
-	public void testAuthenticateThrowsRedirectUrl() throws Exception {
-		String returnUrl = "http://localhost:8080/client";
-		String encodedUrl = URLEncoder.encode(returnUrl, "UTF-8");
-		
-		HttpServletRequest req = mock(HttpServletRequest.class);
-		
-		when(req.getRequestURL()).thenReturn(new StringBuffer(returnUrl));
-		
-//		try {
-//			_oAuth2Auth.authenticate(req);
-//			fail();
-//		} catch (RedirectResponseException ex) {
-//			assertEquals(AUTHLOCATION + "?redirect_uri=" + encodedUrl + "&client_id=" + CLIENTID, ex.getRedirectUrl());
-//		}
 	}
 	
 	
