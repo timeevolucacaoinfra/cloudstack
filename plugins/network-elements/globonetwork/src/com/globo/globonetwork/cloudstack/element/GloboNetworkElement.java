e03deaf69da076f94aece2f2c968fce0441c0020
Silvano Buback <silvano@corp.globo.com>
2014-10-10 18:29:49 -0300

    suporte básico, mas ainda não funcional

--- plugins/network-elements/globonetwork/src/com/globo/globonetwork/cloudstack/element/GloboNetworkElement.java
+++ plugins/network-elements/globonetwork/src/com/globo/globonetwork/cloudstack/element/GloboNetworkElement.java
@@ -25,10 +25,12 @@ import java.util.Set;
 import javax.ejb.Local;
 import javax.inject.Inject;
 
+import org.apache.cloudstack.region.gslb.GslbServiceProvider;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import com.cloud.agent.api.StartupCommand;
+import com.cloud.agent.api.routing.GlobalLoadBalancerConfigCommand;
 import com.cloud.agent.api.to.LoadBalancerTO;
 import com.cloud.dc.DataCenter;
 import com.cloud.dc.DataCenter.NetworkType;
@@ -73,12 +75,12 @@ import com.globo.globonetwork.cloudstack.resource.GloboNetworkResource;
 import com.google.gson.Gson;
 
 @Component
-@Local(value = { NetworkElement.class, LoadBalancingServiceProvider.class })
-public class GloboNetworkElement extends ExternalLoadBalancerDeviceManagerImpl implements LoadBalancingServiceProvider, IpDeployer, ExternalLoadBalancerDeviceManager {
-	private static final Logger s_logger = Logger
-			.getLogger(GloboNetworkElement.class);
+@Local(value = {NetworkElement.class, LoadBalancingServiceProvider.class, GslbServiceProvider.class})
+public class GloboNetworkElement extends ExternalLoadBalancerDeviceManagerImpl implements LoadBalancingServiceProvider, IpDeployer, GslbServiceProvider,
+        ExternalLoadBalancerDeviceManager {
+    private static final Logger s_logger = Logger.getLogger(GloboNetworkElement.class);
 
-	private static final Map<Service, Map<Capability, String>> capabilities = setCapabilities();
+    private static final Map<Service, Map<Capability, String>> capabilities = setCapabilities();
 
     @Inject
     DataCenterDao _dcDao;
@@ -92,19 +94,19 @@ public class GloboNetworkElement extends ExternalLoadBalancerDeviceManagerImpl i
     GloboNetworkService _globoNetworkService;
     @Inject
     ResourceManager _resourceMgr;
-	@Inject
-	HostDao _hostDao;
-	@Inject
-	PhysicalNetworkDao _physicalNetworkDao;
-    
+    @Inject
+    HostDao _hostDao;
+    @Inject
+    PhysicalNetworkDao _physicalNetworkDao;
+
     @Override
-	public Map<Service, Map<Capability, String>> getCapabilities() {
-		return capabilities;
-	}
+    public Map<Service, Map<Capability, String>> getCapabilities() {
+        return capabilities;
+    }
 
-	private static Map<Service, Map<Capability, String>> setCapabilities() {
+    private static Map<Service, Map<Capability, String>> setCapabilities() {
 
-		// Set capabilities for LB service
+        // Set capabilities for LB service
         Map<Capability, String> lbCapabilities = new HashMap<Capability, String>();
         lbCapabilities.put(Capability.SupportedLBAlgorithms, "leastconn, roundrobin");
         lbCapabilities.put(Capability.SupportedLBIsolation, "dedicated, shared");
@@ -115,7 +117,7 @@ public class GloboNetworkElement extends ExternalLoadBalancerDeviceManagerImpl i
 
         // Support inline mode with firewall
         lbCapabilities.put(Capability.InlineMode, "true");
-        
+
         //support only for public lb
         lbCapabilities.put(Capability.LbSchemes, LoadBalancerContainer.Scheme.Public.toString());
 
@@ -132,109 +134,96 @@ public class GloboNetworkElement extends ExternalLoadBalancerDeviceManagerImpl i
 
         // Healthcheck
         lbCapabilities.put(Capability.HealthCheckPolicy, "true");
-        
-		Map<Service, Map<Capability, String>> capabilities = new HashMap<Service, Map<Capability, String>>();
+
+        Map<Service, Map<Capability, String>> capabilities = new HashMap<Service, Map<Capability, String>>();
         capabilities.put(Service.Lb, lbCapabilities);
-		return capabilities;
-	}
+        return capabilities;
+    }
 
-	@Override
-	public Provider getProvider() {
-		return Provider.GloboNetwork;
-	}
+    @Override
+    public Provider getProvider() {
+        return Provider.GloboNetwork;
+    }
 
-	protected boolean canHandle(Network network, Service service) {
-		s_logger.trace("Checking if GloboNetwork can handle service "
-				+ service.getName() + " on network " + network.getDisplayText());
-		
+    protected boolean canHandle(Network network, Service service) {
+        s_logger.trace("Checking if GloboNetwork can handle service " + service.getName() + " on network " + network.getDisplayText());
+
         DataCenter zone = _dcDao.findById(network.getDataCenterId());
-        boolean handleInAdvanceZone = (zone.getNetworkType() == NetworkType.Advanced &&
-                network.getGuestType() == Network.GuestType.Shared && network.getTrafficType() == TrafficType.Guest);
+        boolean handleInAdvanceZone = (zone.getNetworkType() == NetworkType.Advanced && network.getGuestType() == Network.GuestType.Shared && network.getTrafficType() == TrafficType.Guest);
 
         if (!handleInAdvanceZone) {
-            s_logger.trace("Not handling network with Type  " + network.getGuestType() + " and traffic type " + network.getTrafficType() + " in zone of type " + zone.getNetworkType());
+            s_logger.trace("Not handling network with Type  " + network.getGuestType() + " and traffic type " + network.getTrafficType() + " in zone of type "
+                    + zone.getNetworkType());
             return false;
         }
 
-        return (_networkManager.isProviderForNetwork(getProvider(), network.getId()) && _ntwkSrvcDao.canProviderSupportServiceInNetwork(network.getId(), service, Network.Provider.GloboNetwork));
-	}
+        return (_networkManager.isProviderForNetwork(getProvider(), network.getId()) && _ntwkSrvcDao.canProviderSupportServiceInNetwork(network.getId(), service,
+                Network.Provider.GloboNetwork));
+    }
 
-	@Override
-	public boolean implement(Network network, NetworkOffering offering,
-			DeployDestination dest, ReservationContext context)
-			throws ConcurrentOperationException, ResourceUnavailableException,
-			InsufficientCapacityException {
-		return true;
-	}
+    @Override
+    public boolean implement(Network network, NetworkOffering offering, DeployDestination dest, ReservationContext context) throws ConcurrentOperationException,
+            ResourceUnavailableException, InsufficientCapacityException {
+        return true;
+    }
 
-	@Override
-	public boolean prepare(Network network, NicProfile nic,
-			VirtualMachineProfile vm,
-			DeployDestination dest, ReservationContext context)
-			throws ConcurrentOperationException, ResourceUnavailableException,
-			InsufficientCapacityException {
-		return true;
-	}
+    @Override
+    public boolean prepare(Network network, NicProfile nic, VirtualMachineProfile vm, DeployDestination dest, ReservationContext context) throws ConcurrentOperationException,
+            ResourceUnavailableException, InsufficientCapacityException {
+        return true;
+    }
 
-	@Override
-	public boolean release(Network network, NicProfile nic,
-			VirtualMachineProfile vm,
-			ReservationContext context) throws ConcurrentOperationException,
-			ResourceUnavailableException {
-		// Even though removing a VM will clean up load balancing resources later on,
-	    // we have to make sure VM is removed from the load balancer in GloboNetwork
-	    // before attempting to remove its NIC
-	    return _lbManager.removeVmFromLoadBalancers(vm.getId());
-	}
+    @Override
+    public boolean release(Network network, NicProfile nic, VirtualMachineProfile vm, ReservationContext context) throws ConcurrentOperationException, ResourceUnavailableException {
+        // Even though removing a VM will clean up load balancing resources later on,
+        // we have to make sure VM is removed from the load balancer in GloboNetwork
+        // before attempting to remove its NIC
+        return _lbManager.removeVmFromLoadBalancers(vm.getId());
+    }
 
-	@Override
-	public boolean shutdown(Network network, ReservationContext context,
-			boolean cleanup) throws ConcurrentOperationException,
-			ResourceUnavailableException {
-		return true;
-	}
+    @Override
+    public boolean shutdown(Network network, ReservationContext context, boolean cleanup) throws ConcurrentOperationException, ResourceUnavailableException {
+        return true;
+    }
 
-	@Override
-	public boolean destroy(Network network, ReservationContext context)
-			throws ConcurrentOperationException, ResourceUnavailableException {
-		if (!canHandle(network, Service.Lb)) {
-			return false;
-		}
+    @Override
+    public boolean destroy(Network network, ReservationContext context) throws ConcurrentOperationException, ResourceUnavailableException {
+        if (!canHandle(network, Service.Lb)) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public boolean isReady(PhysicalNetworkServiceProvider provider) {
-		return _globoNetworkService.canEnable(provider.getPhysicalNetworkId());
-	}
+    @Override
+    public boolean isReady(PhysicalNetworkServiceProvider provider) {
+        return _globoNetworkService.canEnable(provider.getPhysicalNetworkId());
+    }
 
-	@Override
-	public boolean shutdownProviderInstances(
-			PhysicalNetworkServiceProvider provider, ReservationContext context)
-			throws ConcurrentOperationException, ResourceUnavailableException {
-    	PhysicalNetwork pNtwk = _physicalNetworkDao.findById(provider.getPhysicalNetworkId());
-    	Host host = _hostDao.findByTypeNameAndZoneId(pNtwk.getDataCenterId(), Provider.GloboNetwork.getName(), Type.L2Networking);
-    	if (host != null) {
-    		_resourceMgr.deleteHost(host.getId(), true, false);
-    	}
-		return true;
-	}
+    @Override
+    public boolean shutdownProviderInstances(PhysicalNetworkServiceProvider provider, ReservationContext context) throws ConcurrentOperationException, ResourceUnavailableException {
+        PhysicalNetwork pNtwk = _physicalNetworkDao.findById(provider.getPhysicalNetworkId());
+        Host host = _hostDao.findByTypeNameAndZoneId(pNtwk.getDataCenterId(), Provider.GloboNetwork.getName(), Type.L2Networking);
+        if (host != null) {
+            _resourceMgr.deleteHost(host.getId(), true, false);
+        }
+        return true;
+    }
 
-	@Override
-	public boolean canEnableIndividualServices() {
-		return true;
-	}
+    @Override
+    public boolean canEnableIndividualServices() {
+        return true;
+    }
 
-	@Override
-	public boolean verifyServicesCombination(Set<Service> services) {
-		return true;
-	}
+    @Override
+    public boolean verifyServicesCombination(Set<Service> services) {
+        return true;
+    }
 
-	@Override
-	public IpDeployer getIpDeployer(Network network) {
+    @Override
+    public IpDeployer getIpDeployer(Network network) {
         return this;
-	}
+    }
 
     @Override
     public boolean applyLBRules(Network config, List<LoadBalancingRule> rules) throws ResourceUnavailableException {
@@ -248,32 +237,49 @@ public class GloboNetworkElement extends ExternalLoadBalancerDeviceManagerImpl i
         return returnValue;
     }
 
-	@Override
-	public boolean validateLBRule(Network network, LoadBalancingRule rule) {
-		return true;
-	}
-
-	@Override
-	public List<LoadBalancerTO> updateHealthChecks(Network network,
-			List<LoadBalancingRule> lbrules) {
-		return null;
-	}
-
-	@Override
-	public boolean applyIps(Network network,
-			List<? extends PublicIpAddress> ipAddress, Set<Service> services)
-			throws ResourceUnavailableException {
+    @Override
+    public boolean validateLBRule(Network network, LoadBalancingRule rule) {
         return true;
-	}
+    }
 
-	@Override
+    @Override
+    public List<LoadBalancerTO> updateHealthChecks(Network network, List<LoadBalancingRule> lbrules) {
+        return null;
+    }
+
+    @Override
+    public boolean applyIps(Network network, List<? extends PublicIpAddress> ipAddress, Set<Service> services) throws ResourceUnavailableException {
+        return true;
+    }
+
+    @Override
     public HostVO createHostVOForDirectConnectAgent(HostVO host, final StartupCommand[] startup, ServerResource resource, Map<String, String> details, List<String> hostTags) {
         if (!(startup[0] instanceof StartupCommand && resource instanceof GloboNetworkResource)) {
             return null;
         }
         host.setType(Host.Type.L2Networking);
         return host;
-	}
+    }
 
-	
+    @Override
+    public boolean isServiceEnabledInZone(long zoneId, long physicalNetworkId) {
+        return _globoNetworkService.canEnable(physicalNetworkId);
+    }
+
+    @Override
+    public String getZoneGslbProviderPublicIp(long zoneId, long physicalNetworkId) {
+        return "1.1.1.1";
+    }
+
+    @Override
+    public String getZoneGslbProviderPrivateIp(long zoneId, long physicalNetworkId) {
+        return "2.2.2.2";
+    }
+
+    @Override
+    public boolean applyGlobalLoadBalancerRule(long zoneId, long physicalNetworkId, GlobalLoadBalancerConfigCommand gslbConfigCmd) throws ResourceUnavailableException {
+        s_logger.info("zoneId=" + zoneId + " physicalNetworkId=" + physicalNetworkId + " gslbConfigCmd=" + gslbConfigCmd);
+        return false;
+    }
+
 }
