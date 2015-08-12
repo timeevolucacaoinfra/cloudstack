f24c6eca4a2e1f9408e4863a075782e118d83bbd
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2015-08-12 16:40:33 -0300

    Fixing variable and method names

--- plugins/network-elements/elastic-loadbalancer/src/com/cloud/network/lb/LoadBalanceRuleHandler.java
+++ plugins/network-elements/elastic-loadbalancer/src/com/cloud/network/lb/LoadBalanceRuleHandler.java
@@ -343,7 +343,7 @@ public class LoadBalanceRuleHandler {
             lb.setSourceIpAddressId(ipId);
 
             result = _lbMgr.createPublicLoadBalancer(lb.getXid(), lb.getName(), lb.getDescription(), lb.getSourcePortStart(), lb.getDefaultPortStart(), ipId.longValue(),
-                    lb.getProtocol(), lb.getAlgorithm(), false, CallContext.current(), lb.getLbProtocol(), true, null, lb.getCache(), lb.getServiceDownAction(), lb.getHeathCheckDestination());
+                    lb.getProtocol(), lb.getAlgorithm(), false, CallContext.current(), lb.getLbProtocol(), true, null, lb.getCache(), lb.getServiceDownAction(), lb.getHealthCheckDestination());
         } catch (NetworkRuleConflictException e) {
             s_logger.warn("Failed to create LB rule, not continuing with ELB deployment");
             if (newIp) {
