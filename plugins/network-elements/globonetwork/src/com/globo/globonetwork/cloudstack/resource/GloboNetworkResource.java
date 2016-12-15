fbbb5f38605af2b734ec270735de7282816aa862
victor-mendes-eduardo <victor.mendes.eduardo@gmail.com>
2016-12-15 11:06:37 -0200

    Finding pool by port and name instead of just name. Fixing case when there is more than on pool for each port.

--- plugins/network-elements/globonetwork/src/com/globo/globonetwork/cloudstack/resource/GloboNetworkResource.java
+++ plugins/network-elements/globonetwork/src/com/globo/globonetwork/cloudstack/resource/GloboNetworkResource.java
@@ -893,10 +893,10 @@ public class GloboNetworkResource extends ManagerBase implements ServerResource 
         return lbAlgorithm;
     }
 
-    protected PoolV3 findPoolByPort(Integer port, List<PoolV3> pools) {
+    protected PoolV3 findPoolByPortAndName(Integer port, List<PoolV3> pools, String poolName) {
         if(pools != null) {
             for (PoolV3 pool : pools) {
-                if (port.equals(pool.getDefaultPort())) {
+                if (port.equals(pool.getDefaultPort()) && pool.getIdentifier().equals(poolName)) {
                     return pool;
                 }
             }
@@ -1054,7 +1054,9 @@ public class GloboNetworkResource extends ManagerBase implements ServerResource 
             if(poolIds != null && !poolIds.isEmpty()){
                 pools = poolAPI.getByIdsV3(poolIds);
             }
-            PoolV3 poolV3 = findPoolByPort(realPort, pools);
+
+            String poolName = buildPoolName(cmd.getRegion(), cmd.getHost(), vipPort, realPort);
+            PoolV3 poolV3 = findPoolByPortAndName(realPort, pools, poolName);
 
             //case when user add or remove real, does not need to update pool settings, only reals
             if (poolV3 != null) {
